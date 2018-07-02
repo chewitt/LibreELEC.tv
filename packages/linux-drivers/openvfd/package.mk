7853ac3c6274e6f3ef32a0d97d556fc4364d9817
chewitt <github@chrishewitt.net>
2018-07-02 03:25:43 +0100

    project: add openvfd package *needs mainline port*

---
+++ packages/linux-drivers/openvfd/package.mk
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018 Arthur Liberman (arthur_liberman (at) hotmail.com)
+
+PKG_NAME="openvfd"
+PKG_VERSION="2df5162"
+PKG_SHA256="ad14b276afe51fcfe4e48421941ec7b72f5372ccf69a409695a5f8cd8bd8bd87"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/arthur-liberman/linux_openvfd"
+PKG_URL="https://github.com/arthur-liberman/linux_openvfd/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="linux_openvfd-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_SECTION="driver"
+PKG_LONGDESC="openvfd: Driver for VFD displays"
+PKG_TOOLCHAIN="manual"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make ARCH=$TARGET_KERNEL_ARCH \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       -C "$(kernel_path)" M="$PKG_BUILD/driver"
+    make OpenVFDService
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+
+  mkdir -p $INSTALL/usr/sbin
+    cp -P OpenVFDService $INSTALL/usr/sbin
+}
+
+post_install() {
+  enable_service openvfd.service
+}
