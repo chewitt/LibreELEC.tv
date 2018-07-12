f2d10b97ea6f5d1b367c77c19700995d4551e1bd
chewitt <github@chrishewitt.net>
2018-07-12 13:26:16 +0100

    firmware: adapt brcmfmac_sdio-firmware-aml package

---
+++ packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
@@ -0,0 +1,59 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      COpyright (C) 2018-present Team LibreELEC
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="brcmfmac_sdio-firmware-aml"
+PKG_VERSION="734777e"
+PKG_SHA256=""
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/chewitt/brcmfmac_sdio-firmware-aml"
+PKG_URL="https://github.com/chewitt/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_SECTION="firmware"
+PKG_LONGDESC="Broadcom SDIO firmware used with LibreELEC mainline kernels"
+PKG_TOOLCHAIN="manual"
+
+post_makeinstall_target() {
+  FW_TARGET_DIR=$INSTALL/$(get_full_firmware_dir)
+
+  if find_file_path firmwares/$PKG_NAME.dat; then
+    FW_LISTS="${FOUND_PATH}"
+  else
+    FW_LISTS="${PKG_DIR}/firmwares/any.dat ${PKG_DIR}/firmwares/${TARGET_ARCH}.dat"
+  fi
+
+  for fwlist in ${FW_LISTS}; do
+    [ -f ${fwlist} ] || continue
+    while read -r fwline; do
+      [ -z "${fwline}" ] && continue
+      [[ ${fwline} =~ ^#.* ]] && continue
+      [[ ${fwline} =~ ^[[:space:]] ]] && continue
+
+      for fwfile in $(cd ${PKG_BUILD} && eval "find ${fwline}"); do
+        [ -d ${PKG_BUILD}/${fwfile} ] && continue
+        if [ -f ${PKG_BUILD}/${fwfile} ]; then
+          mkdir -p $(dirname ${FW_TARGET_DIR}/brcm/${fwfile})
+            cp -Lv ${PKG_BUILD}/${fwfile} ${FW_TARGET_DIR}/brcm/${fwfile}
+        else
+          echo "ERROR: Firmware file ${fwfile} does not exist - aborting"
+          exit 1
+        fi
+      done
+    done < ${fwlist}
+  done
+}
