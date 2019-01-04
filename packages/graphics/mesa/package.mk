73b0eb2eaefc30cdcce5a55c00797a05fdfca9e1
chewitt <github@chrishewitt.net>
2019-01-04 13:22:31 +0000

    mesa: support lima/panfrost build options

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,15 +3,34 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="19.0.1"
-PKG_SHA256="58576fd0126f57d6aeacce384b0a5570d442950e8ed15d35bf84e8eb901d65b0"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
-PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
 PKG_BUILD_FLAGS="+lto"
+
+case $PROJECT in
+  Amlogic)
+    case $MALI_FAMILY in
+      t820) # panfrost
+        PKG_VERSION="e4e6a3deaff4f84f0fb99b4dec950dc498d507ed" # master-19.1
+        PKG_SHA256="8d51ec696f0c115061d688e895142fe43ddac4f5f0a9966d3a00225ee44639c5"
+        PKG_URL="https://gitlab.freedesktop.org/mesa/mesa/-/archive/$PKG_VERSION/mesa-$PKG_VERSION.tar.gz"
+        ;;
+      *) # lima
+        PKG_VERSION="9194f141c407da21291b7b3f3a1a20943172c01c" # lima-master-upstream-v2 (19.1)
+        PKG_SHA256="f8b1659592b8d657c0dc2374af7cf9c22d8037c2cb3e2d009d9c1695a43199d9"
+	PKG_URL="https://gitlab.freedesktop.org/yuq825/mesa/-/archive/$PKG_VERSION/mesa-$PKG_VERSION.tar.gz"
+	;;
+    esac
+    ;;
+  *)
+    PKG_VERSION="19.0.1"
+    PKG_SHA256="58576fd0126f57d6aeacce384b0a5570d442950e8ed15d35bf84e8eb901d65b0"
+    PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
+    ;;
+esac
 
 get_graphicdrivers
 
