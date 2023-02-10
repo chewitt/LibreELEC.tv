4f0e13fc2e205b5ab8fa16fc99c0985e962a4edb
Christian Hewitt <christianshewitt@gmail.com>
2023-02-10 05:17:23 +0000

    linux-drivers: add RTL8189ES driver

---
+++ packages/linux-drivers/RTL8189ES/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="RTL8189ES"
+PKG_VERSION="eb51e021b0e1b6f94a4b49da3f4ee5c5fb20b715"
+PKG_SHA256="b28c15f4631f4151ea6fca96d59aec931766af49ccb7a0d5e1080fc41a323713"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/jwrdegoede/rtl8189ES_linux"
+PKG_URL="https://github.com/jwrdegoede/rtl8189ES_linux/archive/${PKG_VERSION}.tar.gz"
+PKG_LONGDESC="Realtek RTL8189ES and RTL8189ETV Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=${TARGET_KERNEL_ARCH} \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+}
