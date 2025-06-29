4298ac29cb55db73616002f07fa095db27e93ffe
Christian Hewitt <christianshewitt@gmail.com>
2025-06-29 14:37:35 +0000

    tm16xx: move package to sysutils and pick only utils

--- packages/sysutils/tm16xx/package.mk
+++ packages/sysutils/tm16xx/package.mk
@@ -7,20 +7,15 @@ PKG_SHA256="e8fbbc5c8c36b3eb42f1f8d7c907106ccf3a2b5af4b2382c6ca4ea63e07fde70"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/jefflessard/tm16xx-display/"
 PKG_URL="https://github.com/jefflessard/tm16xx-display/archive/${PKG_VERSION}.tar.gz"
-PKG_LONGDESC="Linux kernel driver for auxiliary displays using TM1628 compatible controllers"
+PKG_LONGDESC="Linux kernel driver utilities for TM16XX compatible controllers"
 PKG_IS_KERNEL_PKG="yes"
+PKG_TOOLCHAIN="manual"
 
 pre_make_target() {
-  unset LDFLAGS
+  sed -i '/ExecStartPre/d' display.service
 }
 
-make_target() {
-  kernel_make V=1 KDIR=$(kernel_path) module
-}
-
-makeinstall_target() {
-  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
-    cp *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+post_makeinstall_target() {
   mkdir -p ${INSTALL}/usr/sbin
     cp display-service ${INSTALL}/usr/sbin
     cp display-utils ${INSTALL}/usr/sbin
