81aa9c9aa7c4f809c999b55209ae77fbd2e8e2e2
Christian Hewitt <christianshewitt@gmail.com>
2025-08-13 15:41:21 +0000

    tm16xx: update package and tweak systemd service start conditions
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/sysutils/tm16xx/package.mk
+++ packages/sysutils/tm16xx/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tm16xx"
-PKG_VERSION="9148a4e12ba6cf4b375a7fd352667f7f27823426"
-PKG_SHA256="e8fbbc5c8c36b3eb42f1f8d7c907106ccf3a2b5af4b2382c6ca4ea63e07fde70"
+PKG_VERSION="f3bbd1cd22a44cca9a67f2fd6b6aa8346b5f68ef"
+PKG_SHA256="920419d6b61298c3220aff211846dfaa41548807f60642bfe50009e8de5b7c03"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/jefflessard/tm16xx-display/"
 PKG_URL="https://github.com/jefflessard/tm16xx-display/archive/${PKG_VERSION}.tar.gz"
@@ -12,13 +12,13 @@ PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
 
 pre_make_target() {
+  sed -i '/Description=*./a\ConditionPathIsDirectory=/sys/class/leds/display' display.service
   sed -i '/ExecStartPre/d' display.service
 }
 
 post_makeinstall_target() {
   mkdir -p ${INSTALL}/usr/sbin
     cp display-service ${INSTALL}/usr/sbin
-    cp display-utils ${INSTALL}/usr/sbin
   mkdir -p ${INSTALL}/usr/lib/systemd/system
     cp display.service ${INSTALL}/usr/lib/systemd/system
 }
