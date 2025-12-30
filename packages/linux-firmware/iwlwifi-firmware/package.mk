300b7b05ee915cd9a93048ecd10d007d44e02779
Christian Hewitt <christianshewitt@gmail.com>
2025-12-30 04:14:45 +0000

    iwlwifi-firmware: update to f3a2cf9
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="e740611a3f9a7a6673a68ad2a3b9963e7dfdcdbb"
-PKG_SHA256="bbd74b470553836d47700950cef06d4e21bc01c24bcd71227211cff3c4f34b3f"
+PKG_VERSION="f3a2cf9d2477cbeace055d86605464b2cca72247"
+PKG_SHA256="e500a9a3a9a19ceae49c6ef52cdd725a1a2383935d9409b4f6dd02df35d1e25b"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
