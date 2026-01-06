833dc60c36975e47c2df52a0205d5b14926b301f
Christian Hewitt <christianshewitt@gmail.com>
2026-01-06 05:59:21 +0000

    amlogic-boot-fip: use minix-u9h branch for testing
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/tools/amlogic-boot-fip/package.mk
+++ packages/tools/amlogic-boot-fip/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="amlogic-boot-fip"
 PKG_LICENSE="nonfree"
-PKG_VERSION="4e2848a9579e68afe015865309ea6df7096a50e5"
-PKG_SHA256="270803e6ed0148ca62bd72b4a142e8848fff9ffc630b931b5842afeb87320618"
+PKG_VERSION="381476c88e124bd8ae3220c3d52ca3159f38b6f0" # minix-u9-h
+PKG_SHA256="153cc5ceb294549403c1db1bc4bdbf4bf5bf8512e764f3c50e5b86aa2744a692"
 PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
 PKG_URL="https://github.com/LibreELEC/amlogic-boot-fip/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Firmware Image Package (FIP) sources used to sign Amlogic u-boot binaries in LibreELEC images"
