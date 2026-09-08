4f8417de613b16852f9fcd288b5f0a571b647cfe
Christian Hewitt <christianshewitt@gmail.com>
2026-09-08 17:02:03 +0000

    linux: bump Amlogic kernel and patches to Linux 7.3.y
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -16,8 +16,8 @@ PKG_PATCH_DIRS="${LINUX}"
 
 case "${LINUX}" in
   amlogic)
-    PKG_VERSION="8d3ae59288f1e7d58d76558a6ee96d533bc5019f" # 7.2.0
-    PKG_SHA256="2aa2222302771165aa398c30bf66aca1a2433dd1a3b36db43e7157689a090a97"
+    PKG_VERSION="93f51579e7df248780214094418f205253383cc5" # 7.3-rc4
+    PKG_SHA256="4c58bce52b0d316d42bbc60b9602c8a5c24552f803718bf80ecfd23ed0297c5e"
     PKG_URL="https://github.com/torvalds/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="default dvb"
